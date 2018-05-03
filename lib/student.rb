6668c93358fe886278e7b603919fafbfd17cf8f6
Sean Febo <seanfebo@techie.com>
2018-05-03 20:34:56 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,67 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
+  attr_accessor :name, :grade 
+  attr_reader :id 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade 
+    @id = id 
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        grade INTEGER
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students 
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+    if self.id
+      self.update 
+    else
+    sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?) 
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+  end
+  
+  def update 
+    sql = <<-SQL
+      UPDATE students 
+      SET name = ?, grade = ? WHERE id = ?
+      SQL
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
+   end
+   
+   def self.create(name, grade)
+     student = self.new(name, grade)
+     student.save
+     student 
+   end
+   
+   def new_from_db(array)
+     student = student.new 
+     student.id = array[0]
+     student.name = array[1]
+     student.grade = array[2]
+     student 
+   end
+    
 end
+
+
